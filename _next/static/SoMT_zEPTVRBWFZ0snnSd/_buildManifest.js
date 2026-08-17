b3bface660d3a04a28ed4568beae96d04d2124c4
Gisleude <gisleudelinhares@gmail.com>
2026-08-17 17:09:13 -0300

    Deploy static site

---
+++ _next/static/SoMT_zEPTVRBWFZ0snnSd/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/quem-e-o-meu-candidato//_next/:path+",
+        "destination": "/quem-e-o-meu-candidato/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
